b51e2e7f4d1e7c9570480e972174da4dddc0e799
Javier Valles <deliverst@icloud.com>
2023-01-20 15:53:14 -0600

    add file text with the links of the files

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+// details: 
+// date: //
+// by: deliverst
+// todo: 
 const fs = require('fs');
+const execSync = require('child_process').execSync;
 const puppeteer = require('puppeteer');
-let link = "https://downloads.khinsider.com/game-soundtracks/album/super-mario-bros";
+let link = "https://downloads.khinsider.com/game-soundtracks/album/need-for-speed-underground-2";
 
 (async () => {
     //config puppeteer
@@ -9,10 +14,11 @@ let link = "https://downloads.khinsider.com/game-soundtracks/album/super-mario-b
     await page.setViewport({ width: 1920, height: 1080 })
 
     await page.goto(link);
+    
     const linksSecondToPage = await page.evaluate(function() {
         let listLinksRemplaced = []
         let listLinks = document.querySelectorAll(".playlistDownloadSong a")
-        
+
         for (let i = 0; i <= listLinks.length - 1; i++) {
             listLinksRemplaced.push(`${listLinks[i]}.href`)
         }
@@ -21,14 +27,24 @@ let link = "https://downloads.khinsider.com/game-soundtracks/album/super-mario-b
         return listLinksRemplaced.map(link => link.replace(regex, ''))
     })
 
+
     let linksReady = []
+    const titleFolder = await page.evaluate(function() {
+        return document.getElementsByTagName("h2")[0].innerText
+    });
+
+
     for (let p = 0; p <= linksSecondToPage.length - 1; p++) {
         await page.goto(`${linksSecondToPage[p]}`);
         const urlMovie = await page.evaluate(function() {
             return document.getElementById("audio").src
         });
+
         linksReady.push(urlMovie)
-        console.log(urlMovie)
+        execSync(`wget -qq -P "./media/${titleFolder}" ${urlMovie} `, { encoding: 'utf-8' });
     }
+
+    fs.writeFileSync(`./media/${titleFolder}/links.txt`, `${linksReady.toString().replace(/,/g, '\n')}`)
+
     browser.close()
 })()
